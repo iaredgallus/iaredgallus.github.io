de8b951319fc6306252093f3642ae83892da5d8b
iaredgallus <jareddhawn@gmail.com>
2024-08-28 16:21:12 -0500

    Updating resume with more dynamic dropdowns, esp. the writing and research section.

--- resume/script.js
+++ resume/script.js
@@ -4,10 +4,12 @@ function toggleVisibility(id) {
     var arrow = elementBefore.querySelector("td").querySelector("div");
     if (element.style.display === "none") {
         element.style.display = "table-row";
+        element.style.visibility = "visible";
         elementBefore.style.backgroundColor = "#dae7f1";
         arrow.classList.replace("arrow-right", "arrow-down");
     } else {
         element.style.display = "none";
+        element.style.visibility = "hidden";
         element.previousElementSibling.style.backgroundColor = "white";
         arrow.classList.replace("arrow-down", "arrow-right");
     }
