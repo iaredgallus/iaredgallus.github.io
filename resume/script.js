8ddc3a9f29cf3d3aa68ee4d5a5bd7adc9a67d2ae
Iared Gallus <jareddhawn@gmail.com>
2024-09-07 13:39:20 -0500

    Updated JS on resume so dropdowns have something approximating a transition.

--- resume/script.js
+++ resume/script.js
@@ -2,15 +2,23 @@ function toggleVisibility(id) {
     var element = document.getElementById(id);
     var elementBefore = element.previousElementSibling;
     var arrow = elementBefore.querySelector("td").querySelector("div");
-    if (element.style.display === "none") {
-        element.style.display = "table-row";
-        element.style.visibility = "visible";
-        elementBefore.style.backgroundColor = "#dae7f1";
-        arrow.classList.replace("arrow-right", "arrow-down");
+    if (element.classList.contains("visible")) {
+        element.classList.remove("visible");
+        //element.style.visibility = "hidden";
+        elementBefore.style.backgroundColor = "transparent";
+        setTimeout(function() {
+            arrow.classList.replace("arrow-down", "arrow-right");
+            element.style.display = "none";
+        }, 300);
     } else {
-        element.style.display = "none";
-        element.style.visibility = "hidden";
-        element.previousElementSibling.style.backgroundColor = "white";
-        arrow.classList.replace("arrow-down", "arrow-right");
+        element.style.display = "table-row";
+        elementBefore.style.backgroundColor = "#dae7f1";
+        setTimeout(function() {
+            element.classList.add("visible");
+            arrow.classList.replace("arrow-right", "arrow-down");
+        }, 100);
+        //element.style.visibility = "visible";
+        //element.style.opacity = "1";
+        //element.style.color = "#0e1b25";
     }
 }
