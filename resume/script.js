d20061debffa9bfc4da32390f6764cf52e5ae90a
iaredgallus <jareddhawn@gmail.com>
2024-09-07 19:30:38 -0500

    Fiddled with resume dropdown animation to achieve something more appealing.

--- resume/script.js
+++ resume/script.js
@@ -1,24 +1,23 @@
 function toggleVisibility(id) {
     var element = document.getElementById(id);
     var elementBefore = element.previousElementSibling;
+    var child = element.firstElementChild;
     var arrow = elementBefore.querySelector("td").querySelector("div");
     if (element.classList.contains("visible")) {
+        // TURN DISPLAY OFF
         element.classList.remove("visible");
-        //element.style.visibility = "hidden";
         elementBefore.style.backgroundColor = "transparent";
         setTimeout(function() {
             arrow.classList.replace("arrow-down", "arrow-right");
-            element.style.display = "none";
-        }, 300);
+            child.style.display = "none";
+        }, 200);
     } else {
-        element.style.display = "table-row";
+        // TURN DISPLAY ON
+        child.style.display = "table-cell";
         elementBefore.style.backgroundColor = "#dae7f1";
-        setTimeout(function() {
-            element.classList.add("visible");
+        element.classList.add("visible");
+        setTimeout(function () {
             arrow.classList.replace("arrow-right", "arrow-down");
-        }, 100);
-        //element.style.visibility = "visible";
-        //element.style.opacity = "1";
-        //element.style.color = "#0e1b25";
+        }, 200)
     }
 }
